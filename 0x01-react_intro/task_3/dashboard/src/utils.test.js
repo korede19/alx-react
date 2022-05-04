70872069ae377dfa225c15e54e6bde6e4c0e15ef
unknown <oyeyemikorede5@gmail.com>
2022-05-05 00:53:39 +0100

    react

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,8 @@
+ 283 Bytes
+   
+export const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) =>
+              isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
